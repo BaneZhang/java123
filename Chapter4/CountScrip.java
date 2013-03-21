1d039491625efd3ab76f439eb5910efd07114728
Bane <banezhang@zoho.com>
2013-03-21 23:27:33 +0800

    第4章练习题

---
+++ Chapter4/CountScrip.java
@@ -0,0 +1,12 @@
+public class CountScrip {
+	public static void main(String[] args) {
+		int totalCost = 350;
+		if (totalCost < 100) {
+			System.out.println("购物金额不足100元，不赠送抵价劵。");
+		} else if (totalCost <= 500) {
+			System.out.println("购物金额满100元，赠送55元抵价劵。");
+		} else {
+			System.out.println("购物金额满500元，赠送155元抵价劵。");
+		}
+	}
+}
